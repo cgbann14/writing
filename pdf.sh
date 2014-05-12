5fd035a9c2de745f355518a9984c2e9e3a35b958
Neel Smith <dnsmith.neel@gmail.com>
2014-05-12 06:22:49 -0400

    Correct typo

--- pdf.sh
+++ pdf.sh
@@ -1,4 +1,5 @@
 # Follow steps in Checklist.md, then run this in 
 # flat markdown directory:
 
-pandoc --latex-engine=xelatex  --variable documentclass="book" --variable monofont="Courier"  --variable mainfont="Gentium"   --variable sansfont="Seravek" --toc --bibliography="phoros.bib"  --variable date="May 12, 2014" --variable author="Christine Bannan" --variable="A Reassessment of Athenian Tribute" -o thesis.pdf `cat Books.txt`
+pandoc --latex-engine=xelatex  --variable documentclass="book" --variable monofont="Courier"  --variable mainfont="Gentium"   --variable sansfont="Seravek" --toc --bibliography="phoros.bib"  --variable date="May 12, 2014" --variable author="Christine Bannan" --variable title="A Reassessment of Athenian Tribute" -o thesis.pdf `cat Books.txt` --template=pandoc-template.tex
+
